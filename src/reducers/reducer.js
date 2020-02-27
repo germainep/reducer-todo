783464540682820a0d2a738316b9049e63ba0381
Germaine Pannell <me@germainep.com>
2020-02-26 19:30:54 -0500

    Created files for the reducers and todo components

---
+++ src/reducers/reducer.js
@@ -0,0 +1,13 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+
+    case 'ADDTODO':
+      return {...state, }
+    case 'CLEARCOMPLETED':
+      return {...state, action}
+    default:
+      return state
+
+  }
+
+}
